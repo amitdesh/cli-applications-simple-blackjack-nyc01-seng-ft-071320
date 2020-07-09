f99a65b135e26556e3785a88bdd7adb14d291e78
Amit Deshpande <amitdesh2014@gmail.com>
2020-07-09 21:51:17 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -49,7 +49,7 @@ def hit?(initial_hand_total)
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command."
 end
 
 #####################################################
